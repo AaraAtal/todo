a337307782482760e08b516aee22f73a0a8cb037
Atalova Arapat <atalovaarapat@MacBook-Pro-AA.fritz.box>
2021-04-19 11:01:20 +0200

    repied

--- main/models.py
+++ main/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-class ToDo(models.Model):
-    text = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
-    is_done = models.BooleanField(default=False)
-    is_marked = models.BooleanField(default=False)
+
--- todo/urls.py
+++ todo/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path("", homepage, name="hame"),
     path("test/", test, name="test"),
     path("test2/", second),
-    path("test3/", third),
+    
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
